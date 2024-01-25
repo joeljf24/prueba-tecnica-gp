df24b855a7d3dd5c337c9b004cb3d8e5d319775b
joeljf24 <u.programming.jf@gmail.com>
2024-01-25 01:16:56 -0300

    organizing Home and carousel created

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,7 +1,53 @@
+import { useState } from 'react';
+import { images } from '../../utils/data';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 const Carousel = () => {
-    return (
-      <div>Carousel</div>
-    )
-  }
-  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const getIndexes = () => {
+    const prevIndex = (currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+    const nextIndex = (currentIndex === images.length - 1 ? 0 : currentIndex + 1);
+    return { prevIndex, nextIndex };
+  };
+
+  const { prevIndex, nextIndex } = getIndexes();
+
+  const handlePrev = () => {
+    setCurrentIndex(prevIndex);
+  };
+
+  const handleNext = () => {
+    setCurrentIndex(nextIndex);
+  };
+
+  return (
+    <div className='flex items-center'>
+      <div className='relative overflow-hidden items-center max-w-4xl lg:h-[500px] rounded-3xl'>
+        <div>
+          <div className='flex transition-transform ease-out duration-400' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+            {images.map((image, index) => (
+              <img key={index} src={image} alt={`image ${index + 1}`} />
+            ))}
+          </div>
+
+          <div className='absolute inset-0 flex items-center justify-between p-4'>
+            <button onClick={handlePrev} className='p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white'><FaChevronLeft size={16} /></button>
+            <button onClick={handleNext} className='p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white'><FaChevronRight size={16} /></button>
+          </div>
+
+          <div className='absolute bottom-4 right-0 left-0'>
+            <div className='flex items-center justify-center gap-2'>
+              {images.map((_, index) => (
+                <div key={index} className={`transition-all w-3 h-3 bg-white rounded-full ${currentIndex === index ? 'p2' : 'bg-opacity-50'}`} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Carousel;
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,7 +1,38 @@
+import Carousel from '../../components/Carousel/Carousel';
+import { IoIosContacts } from 'react-icons/io';
+import { FaRegFilePdf } from 'react-icons/fa';
+import { IoImagesOutline } from 'react-icons/io5';
+import { NavLink } from 'react-router-dom'
+
 const Home = () => {
-    return (
-      <div>Home</div>
-    )
-  }
-  
+  return (
+    <div className='flex flex-col lg:flex-row lg:items-center justify-center lg:justify-around h-screen md:pt-24 lg:px-10'>
+      <div className='bg-white/10 rounded-3xl mb-10 lg:mb-0'>
+        <div className='flex flex-col text-white lg:text-left p-4 rounded-full backdrop-blur-xl'>
+          <h1 className='text-6xl font-extrabold pb-3'>
+            PENNACloud
+          </h1>
+          <p className='pb-3'>Tu gestor de archivos de confianza</p>
+
+          <div className='flex justify-center gap-8'>
+            <NavLink to='/'>
+              <IoIosContacts size={32} />
+            </NavLink>
+            <NavLink to='/'>
+              <FaRegFilePdf size={32} />
+            </NavLink>
+            <NavLink to='/'>
+              <IoImagesOutline size={32} />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <Carousel />
+      </div>
+    </div>
+  )
+}
+
 export default Home;
